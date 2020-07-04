42ce2b6eb0429cd45eccf8d99dd60c5de11cd837
Russell Salcido <russell.salcido@gmail.com>
2020-07-03 17:52:10 -0700

    Built list of questions

---
+++ assets/questions.js
@@ -0,0 +1,53 @@
+// The array of questions
+var questions = [
+    {
+      title: "Question 1",
+      choice: [
+        "A",
+        "B",
+        "C",
+        "D"
+      ],
+      answer: "Answer 1",
+    },
+    {
+      title: "Question 2",
+      choice: [
+        "A",
+        "B",
+        "C",
+        "D"
+      ],
+      answer: "Answer 2",
+    },
+    {
+      title: "Question 3",
+      choice: [
+        "A",
+        "B",
+        "C",
+        "D"
+      ],
+      answer: "Answer 3",
+    },
+    {
+      title: "Question 4",
+      choice: [
+        "A",
+        "B",
+        "C",
+        "D"
+      ],
+      answer: "Answer 4",
+    },
+    {
+      title: "Question 5",
+      choice: [
+        "A",
+        "B",
+        "C",
+        "D"
+      ],
+      answer: "Answer 5",
+    },
+  ];
